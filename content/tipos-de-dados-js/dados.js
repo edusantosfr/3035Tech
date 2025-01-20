17a109607deb2f6a3bc4d48bda73ea407bdf5c68
edusantosfr <edusantosfr8584@gmail.com>
2025-01-20 11:50:40 -0300

    commit aula de tipos de dados

---
+++ content/tipos-de-dados-js/dados.js
@@ -0,0 +1,10 @@
+let numero = 0
+let nome = "Eduardo"
+let numeroComVirgula = 0.1
+let boolean = true
+let array = [1, 2, "carro", 'a', true]
+let objeto = {modelo:"uno fire 1.4", marca:"fiat", ano: 2001}
+let variavel
+let variavelNull = null
+
+let concatenar = nome + " " + sobreNome
